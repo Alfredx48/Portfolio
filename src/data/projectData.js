9df76121e21ce66478d00b52a789e5233af8c631
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-08-30 17:53:38 -0700

    imported images for deployment

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,16 +1,22 @@
+import Mardinis from "/src/assets/mardinis.png";
+import Cuisine from "/src/assets/cuisine-creations.jpeg";
+import SemiGames from "/src/assets/semi-addicting-games.png";
+
+
+
 const projectData = [
     {
-        image: "/src/assets/mardinis.png",
+        image: Mardinis,
         name: "Mardinis Restaurant",
         description: "Created a sleek and user-friendly web application that empowers customers to easily place orders and view menus using technologies such as Ruby on Rails API for the backend and React for the frontend. The platform boasts visually stunning CSS styles and captivating framer-motion animations that enhance the overall experience."
     },
     {
-        image: "/src/assets/cuisine-creations.jpeg",
+        image: Cuisine,
         name: "Cuisine Creations",
         description: "Crafted a dynamic web application that provides a platform for users to discover, share, and create unique recipes using the latest technologies including Ruby on Rails and React. This application allows users to create and edit their own recipes, as well as add ingredients, steps, and measurements. Complete with stylish CSS design, allows users to effortlessly navigate and enjoy the application."
     },
     {
-        image: "/src/assets/semi-addicting-games.png",
+        image: SemiGames,
         name: "Semi-Addicting Games",
         description: "Developed a captivating single-page web application that merges the popular games tic-tac-toe and hangman, with the use of technologies such as React-router-dom, styled-components, and framer-motion animations. The platform has an engaging and intuitive interface with a visually appealing design. "
     },
