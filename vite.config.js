64f30843af624a53100143bd351f421a2b2a735d
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-09-16 22:57:51 -0700

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
-import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   plugins: [
+    /* 
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
     devtools(),
     solid(),
-    copy({
-      targets: [
-        { src: '_redirects', dest: 'dist' }
-      ],
-      hook: 'writeBundle' // important to set this
-    })
   ],
   server: {
     port: 3000,
