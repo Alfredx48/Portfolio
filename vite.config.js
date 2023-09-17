2709e68091ad64372e10708cc5d783775fd7748f
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-09-16 22:37:16 -0700

    added redirects for refresh 404 error bug

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
+import copy from 'vite-plugin-copy';
 
 export default defineConfig({
   plugins: [
-    /* 
+        /* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     devtools(),
     solid(),
+    copy({
+      targets: [
+        { src: '_redirects', dest: 'dist' }
+      ]
+    })
   ],
   server: {
     port: 3000,
